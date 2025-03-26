105d8f0a9374e2f8e1b128073bf3d8898d0548dd
mattiacoli <mattia.coli@aol.com>
2025-03-26 15:07:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-center my-3'>My Blog</h1>
+
+      <div className="container">
+        <ul class="list-group list-group-numbered">
+          <li class="list-group-item">Item</li>
+          <li class="list-group-item">Item</li>
+          <li class="list-group-item">item</li>
+        </ul>
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App
